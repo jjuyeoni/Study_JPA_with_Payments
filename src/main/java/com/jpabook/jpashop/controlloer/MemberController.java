f6e32cf6982fc7f2cc7a951c5bf6e0a678af5769
jjuyeoni <cwa302@naver.com>
2023-05-26 00:26:39 +0900

    make view for user join

---
+++ src/main/java/com/jpabook/jpashop/controlloer/MemberController.java
@@ -0,0 +1,45 @@
+package com.jpabook.jpashop.controlloer;
+
+import com.jpabook.jpashop.domain.Address;
+import com.jpabook.jpashop.domain.Member;
+import com.jpabook.jpashop.service.MemberService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model){
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result){
+        // @Valid 을 쓰면 @NotEmpty validation 걸어둔 것을 사용 가능 (또 다른 validation 들도 사용 가능)
+
+        if(result.hasErrors()){
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+}
